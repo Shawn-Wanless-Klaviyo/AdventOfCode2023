2b70e432398f2bbcbd1c31ece5c3ef27ad104b07
Shawn Wanless <shawn.wanless@klaviyo.com>
2023-12-01 16:16:06 -0500

    next day script and udpated readme

---
+++ make_next_day.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+
+
+def _next_day():
+    days = []
+    for entry in os.scandir("."):
+        if entry.name.isnumeric() and entry.is_dir():
+            days.append(int(entry.name))
+    days.sort()
+    return days[-1] + 1
+
+
+def update_readme(day):
+    with open("README.md", "a") as f:
+        lines = [
+            f"<h2>Day {day}</h2>\n",
+            f"- [Prompt](https://adventofcode.com/2023/day/{day})\n",
+            f"- [Solution](./{day}/solution.py)\n",
+            "\n"
+        ]
+        f.writelines(lines)
+
+
+def make_next_dir(day):
+    os.mkdir(f"./{day}")
+    for entry in os.scandir("./boilerplate"):
+        shutil.copy(entry.path, f"./{day}")
+
+
+if __name__ == '__main__':
+    next_day = _next_day()
+    make_next_dir(next_day)
+    update_readme(next_day)
