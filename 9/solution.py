88f9cc2588b299ecc7f12a18821ce4f22f43c997
Shawn Wanless <shawn.wanless@klaviyo.com>
2023-12-09 02:10:23 -0500

    day 9

--- 9/solution.py
+++ 9/solution.py
@@ -1,31 +1,16 @@
 def part1(input_file, reverse=False):
-    jump = -1 if reverse else 1
     res = 0
     for line in input_file.readlines():
         data = [int(num) for num in line.strip().split(" ")]
-        res += _extrapolate(data[::jump])
+        res += _extrapolate_rec(data[::-1 if reverse else 1])
     return res
 
 
-def _extrapolate(data):
-    sequences = [data]
-    while not _all_same(sequences[-1]):
-        next_sequence = []
-        for i in range(1, len(sequences[-1])):
-            next_sequence.append(sequences[-1][i] - sequences[-1][i - 1])
-        sequences.append(next_sequence)
-    next_num = sequences[-1][-1]
-    for i in range(len(sequences) - 2, -1, -1):
-        sequences[i].append(sequences[i][-1] + next_num)
-        next_num = sequences[i][-1]
-    return next_num
-
-
-def _all_same(sequence):
-    for i in range(1, len(sequence)):
-        if sequence[i] != sequence[i - 1]:
-            return False
-    return True
+def _extrapolate_rec(data):
+    if not any(data):
+        return 0
+    next_num = _extrapolate_rec([data[i] - data[i - 1] for i in range(1, len(data))])
+    return data[-1] + next_num
 
 
 def part2(input_file):
